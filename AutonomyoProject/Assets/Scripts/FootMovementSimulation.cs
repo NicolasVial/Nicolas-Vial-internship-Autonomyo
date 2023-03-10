5a88c4ee1be2939872d6a53c884ef6c641eee1a6
Nicolas Vial <nicolas.vial@epfl.ch>
2023-03-10 13:21:44 +0100

    some cleanup + gitignore changes

--- AutonomyoProject/Assets/Scripts/FootMovementSimulation.cs
+++ AutonomyoProject/Assets/Scripts/FootMovementSimulation.cs
@@ -19,20 +19,40 @@ public class FootMovementSimulation : MonoBehaviour
     private AnimationCurve curve;
 
     [SerializeField]
-    private Vector3 upKneeMovementVector = new Vector3(-0.5f, 0.5f, 0f);
+    private Vector3 upKneeMovementVector;
 
-    private float elapsedTimeLeft = 0;
-    private float elapsedTimeRight = 0;
-    private Vector3 initialPosLeft;
-    private Vector3 finalPosLeft;
-    private Vector3 initialPosRight;
-    private Vector3 finalPosRight;
-    private bool isMovingBackwardLeft = false;
-    private bool isMovingBackwardRight = false;
-    private bool isMovingForwardRight = false;
-    private bool isMovingForwardLeft = false;
-    private float percentageCompleteLeft;
-    private float percentageCompleteRight;
+    [SerializeField]
+    private Vector3 abductionRightVector;
+
+    [SerializeField]
+    private Vector3 abductionLeftVector;
+
+    private bool isLeftMoving = false;
+    private bool isRightMoving = false;
+    private float elapsedTimeUpKneeRight = 0;
+    private float elapsedTimeUpKneeLeft = 0;
+    private float elapsedTimeAbductionLeft = 0;
+    private float elapsedTimeAbductionRight = 0;
+    private Vector3 initialPosUpKneeLeft;
+    private Vector3 finalPosUpKneeLeft;
+    private Vector3 initialPosUpKneeRight;
+    private Vector3 finalPosUpKneeRight;
+    private Vector3 initialPosAbductionLeft;
+    private Vector3 finalPosAbductionLeft;
+    private Vector3 initialPosAbductionRight;
+    private Vector3 finalPosAbductionRight;
+    private bool isMovingBackwardUpKneeLeft = false;
+    private bool isMovingBackwardUpKneeRight = false;
+    private bool isMovingForwardUpKneeRight = false;
+    private bool isMovingForwardUpKneeLeft = false;
+    private bool isMovingBackwardAbductionLeft = false;
+    private bool isMovingBackwardAbductionRight = false;
+    private bool isMovingForwardAbductionRight = false;
+    private bool isMovingForwardAbductionLeft = false;
+    private float percentageCompleteUpKneeLeft;
+    private float percentageCompleteUpKneeRight;
+    private float percentageCompleteAbductionLeft;
+    private float percentageCompleteAbductionRight;
 
     // Start is called before the first frame update
     void Start()
@@ -43,23 +63,38 @@ public class FootMovementSimulation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        startMovementListener("ToggleUpKneeLeft", ref isMovingForwardLeft, isMovingBackwardLeft, ref initialPosLeft, ref finalPosLeft, leftFoot, upKneeMovementVector);
-        startMovementListener("ToggleUpKneeRight", ref isMovingForwardRight, isMovingBackwardRight, ref initialPosRight, ref finalPosRight, rightFoot, upKneeMovementVector);
+        startMovementListener("ToggleUpKneeLeft", ref isMovingForwardUpKneeLeft, isMovingBackwardUpKneeLeft, ref initialPosUpKneeLeft, ref finalPosUpKneeLeft, leftFoot, upKneeMovementVector, true);
+        startMovementListener("ToggleUpKneeRight", ref isMovingForwardUpKneeRight, isMovingBackwardUpKneeRight, ref initialPosUpKneeRight, ref finalPosUpKneeRight, rightFoot, upKneeMovementVector, false);
+        startMovementListener("ToggleAbductionLeft", ref isMovingForwardAbductionLeft, isMovingBackwardAbductionLeft, ref initialPosAbductionLeft, ref finalPosAbductionLeft, leftFoot, abductionLeftVector, true);
+        startMovementListener("ToggleAbductionRight", ref isMovingForwardAbductionRight, isMovingBackwardAbductionRight, ref initialPosAbductionRight, ref finalPosAbductionRight, rightFoot, abductionRightVector, false);
 
-        isMovingForwardLeft = executeForwardMovement(isMovingForwardLeft, ref isMovingBackwardLeft, ref elapsedTimeLeft, ref percentageCompleteLeft, ref leftFoot, ref initialPosLeft, ref finalPosLeft, upKneeMovementVector);
-        isMovingForwardRight = executeForwardMovement(isMovingForwardRight, ref isMovingBackwardRight, ref elapsedTimeRight, ref percentageCompleteRight, ref rightFoot, ref initialPosRight, ref finalPosRight, upKneeMovementVector);
+        isMovingForwardUpKneeLeft = executeForwardMovement(isMovingForwardUpKneeLeft, ref isMovingBackwardUpKneeLeft, ref elapsedTimeUpKneeLeft, ref percentageCompleteUpKneeLeft, ref leftFoot, ref initialPosUpKneeLeft, ref finalPosUpKneeLeft, upKneeMovementVector);
+        isMovingForwardUpKneeRight = executeForwardMovement(isMovingForwardUpKneeRight, ref isMovingBackwardUpKneeRight, ref elapsedTimeUpKneeRight, ref percentageCompleteUpKneeRight, ref rightFoot, ref initialPosUpKneeRight, ref finalPosUpKneeRight, upKneeMovementVector);
+        isMovingForwardAbductionLeft = executeForwardMovement(isMovingForwardAbductionLeft, ref isMovingBackwardAbductionLeft, ref elapsedTimeAbductionLeft, ref percentageCompleteAbductionLeft, ref leftFoot, ref initialPosAbductionLeft, ref finalPosAbductionLeft, abductionLeftVector);
+        isMovingForwardAbductionRight = executeForwardMovement(isMovingForwardAbductionRight, ref isMovingBackwardAbductionRight, ref elapsedTimeAbductionRight, ref percentageCompleteAbductionRight, ref rightFoot, ref initialPosAbductionRight, ref finalPosAbductionRight, abductionRightVector);
 
-        isMovingBackwardLeft = executeBackwardMovement(isMovingBackwardLeft, ref elapsedTimeLeft, ref percentageCompleteLeft, ref leftFoot, initialPosLeft, finalPosLeft);
-        isMovingBackwardRight = executeBackwardMovement(isMovingBackwardRight, ref elapsedTimeRight, ref percentageCompleteRight, ref rightFoot, initialPosRight, finalPosRight);
+        isMovingBackwardUpKneeLeft = executeBackwardMovement(isMovingBackwardUpKneeLeft, ref elapsedTimeUpKneeLeft, ref percentageCompleteUpKneeLeft, ref leftFoot, initialPosUpKneeLeft, finalPosUpKneeLeft, true);
+        isMovingBackwardUpKneeRight = executeBackwardMovement(isMovingBackwardUpKneeRight, ref elapsedTimeUpKneeRight, ref percentageCompleteUpKneeRight, ref rightFoot, initialPosUpKneeRight, finalPosUpKneeRight, false);
+        isMovingBackwardAbductionLeft = executeBackwardMovement(isMovingBackwardAbductionLeft, ref elapsedTimeAbductionLeft, ref percentageCompleteAbductionLeft, ref leftFoot, initialPosAbductionLeft, finalPosAbductionLeft, true);
+        isMovingBackwardAbductionRight = executeBackwardMovement(isMovingBackwardAbductionRight, ref elapsedTimeAbductionRight, ref percentageCompleteAbductionRight, ref rightFoot, initialPosAbductionRight, finalPosAbductionRight, false);
     }
 
-    private void startMovementListener(string toggleString, ref bool isMovingForward, bool isMovingBackward, ref Vector3 initialPos, ref Vector3 finalPos, GameObject foot, Vector3 movementvector)
+    private void startMovementListener(string toggleString, ref bool isMovingForward, bool isMovingBackward, ref Vector3 initialPos, ref Vector3 finalPos, GameObject foot, Vector3 movementvector, bool isLeft)
     {
-        if (InputManager.GetButtonDown(toggleString) && !isMovingForward && !isMovingBackward)
+        if (InputManager.GetButtonDown(toggleString) && !isMovingForward && !isMovingBackward && ((isLeft == true && isLeftMoving == false) || (isLeft == false && isRightMoving == false)))
         {
             isMovingForward = true;
             initialPos = foot.transform.position;
             finalPos = new Vector3(foot.transform.position.x + movementvector.x, foot.transform.position.y + movementvector.y, foot.transform.position.z + movementvector.z);
+            
+            if (isLeft)
+            {
+                isLeftMoving = true;
+            }
+            else
+            {
+                isRightMoving = true;
+            }
         }
     }
 
@@ -84,7 +119,7 @@ public class FootMovementSimulation : MonoBehaviour
         
     }
 
-    private bool executeBackwardMovement(bool isMovingBackward, ref float elapsedTime, ref float percentageComplete, ref GameObject foot, Vector3 initialPos, Vector3 finalPos)
+    private bool executeBackwardMovement(bool isMovingBackward, ref float elapsedTime, ref float percentageComplete, ref GameObject foot, Vector3 initialPos, Vector3 finalPos, bool isLeft)
     {
         if (isMovingBackward)
         {
@@ -94,6 +129,14 @@ public class FootMovementSimulation : MonoBehaviour
             if (percentageComplete >= 1)
             {     
                 elapsedTime = 0;
+                if (isLeft)
+                {
+                    isLeftMoving = false;
+                }
+                else
+                {
+                    isRightMoving = false;
+                }
                 return false;
             }
             return true;
